2edd6c287d9e8626e3ff7d5f5351ebb5bc51e9cb
Fernando <Fernando@DESKTOP-3EE8PP0.home>
2023-03-18 16:51:09 +0100

    Por hacer

---
+++ Pruebas_De_Caja_Blanca/src/clases/SumaNumerosTest.java
@@ -0,0 +1,14 @@
+package clases;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class SumaNumerosTest {
+
+	@Test
+	void testSumaNumeros() {
+		fail("Not yet implemented");
+	}
+
+}
