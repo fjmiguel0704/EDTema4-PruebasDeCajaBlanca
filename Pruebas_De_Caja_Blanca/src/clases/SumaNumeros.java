af96dc5fe6081dfc5708eeeb5ed840a8f84bce10
fmigg <fmigg@Fernando>
2023-03-19 23:14:36 +0100

    Terminado

--- Pruebas_De_Caja_Blanca/src/clases/SumaNumeros.java
+++ Pruebas_De_Caja_Blanca/src/clases/SumaNumeros.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class SumaNumeros {
 	public int sumaNumeros(int num) {
 		int suma = 0;
-		for (int i = 1; i < num; i++) {
+		for (int i = 1; i <= num; i++) {
 			if(i%2==0) {
 				suma += i;
 			}
--- Pruebas_De_Caja_Blanca/src/clases/SumaNumerosTest.java
+++ Pruebas_De_Caja_Blanca/src/clases/SumaNumerosTest.java
@@ -2,13 +2,32 @@ package clases;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.stream.Stream;
+
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 class SumaNumerosTest {
 
-	@Test
-	void testSumaNumeros() {
-		fail("Not yet implemented");
+	@ParameterizedTest
+	@MethodSource("sumaNumeros")
+	void testSumaNumeros(int num, int expected) {
+		SumaNumeros suma = new SumaNumeros ();
+		
+		int resultado = suma.sumaNumeros(num);
+		
+		assertEquals(expected, resultado);
+	}
+	
+	private static Stream <Arguments> sumaNumeros (){
+		return Stream.of(
+				Arguments.of(0, 0),
+				Arguments.of(3, 2),
+				Arguments.of(2, 2),
+				Arguments.of(8, 20)
+				);
 	}
 
 }
